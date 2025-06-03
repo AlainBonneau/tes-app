4e1a43883fd1459049ffbfe69d4f5eb8e1a174d6
Alain Bonneau <alaindev97@gmail.com>
2025-06-03 03:59:25 +0200

    add get race by id controller

--- tes-backend/src/controllers/racesController.ts
+++ tes-backend/src/controllers/racesController.ts
@@ -1,6 +1,15 @@
 import { FastifyRequest, FastifyReply } from "fastify";
 
 // Début du typage
+type GetByIdRequest = FastifyRequest<{ Params: { id: string } }>;
+
+type CreateRaceRequest = FastifyRequest<{
+  Body: {
+    name: string;
+    faction: string;
+    description: string;
+  };
+}>;
 
 // Fin du typage
 
@@ -13,6 +22,25 @@ export async function getAllRaces(
     reply.send(races);
   } catch (error) {
     console.error("Erreur lors de la récupératino des races", error);
+    reply.status(500).send({ error: "Erreur interne du serveur" });
+  }
+}
+
+export async function getRaceById(
+  request: GetByIdRequest,
+  reply: FastifyReply
+) {
+  try {
+    const id = parseInt(request.params.id, 10);
+    const race = await request.server.prisma.race.findUnique({ where: { id } });
+
+    if (!race) {
+      return reply.status(404).send({ error: "Race introuvable" });
+    }
+
+    reply.send(race);
+  } catch (error) {
+    console.error("Erreur lors de la récupération de la race :", error);
     reply.status(500).send({ error: "Erreur interne du serveur" });
   }
 }
