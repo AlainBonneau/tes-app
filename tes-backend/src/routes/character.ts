306ddd9fa1b42daf2a58cfa433faa8206594266c
Alain Bonneau <alaindev97@gmail.com>
2025-06-04 03:10:01 +0200

    add all characters routes

---
+++ tes-backend/src/routes/character.ts
@@ -0,0 +1,33 @@
+import { FastifyInstance } from "fastify";
+import {
+  getAllCharacters,
+  getCharacterById,
+  createCharacter,
+  updateCharacter,
+  deleteCharacter,
+} from "../controllers/charactersController";
+
+export default async function (app: FastifyInstance) {
+  app.get("/caracteres", getAllCharacters);
+  app.get<{ Params: { id: string } }>("/caracteres/:id", getCharacterById);
+  app.post<{
+    Body: {
+      name: string;
+      description: string;
+      imageUrl?: string;
+      regionId: number;
+      raceId: number;
+    };
+  }>("/caracteres", createCharacter);
+  app.patch<{
+    Params: { id: string };
+    Body: {
+      name: string;
+      description: string;
+      imageUrl?: string;
+      regionId: number;
+      raceId: number;
+    };
+  }>("/caracteres/:id", updateCharacter);
+  app.delete<{ Params: { id: string } }>("/caracteres/:id", deleteCharacter);
+}
