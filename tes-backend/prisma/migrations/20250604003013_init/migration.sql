518230aba4d22051107739c75440d4907a87eb91
Alain Bonneau <alaindev97@gmail.com>
2025-06-04 02:45:13 +0200

    ad create character controler

---
+++ tes-backend/prisma/migrations/20250604003013_init/migration.sql
@@ -0,0 +1,52 @@
+-- CreateTable
+CREATE TABLE "Region" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "imageUrl" TEXT,
+
+    CONSTRAINT "Region_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Race" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "origine" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+
+    CONSTRAINT "Race_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Character" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "imageUrl" TEXT,
+    "regionId" INTEGER NOT NULL,
+    "raceId" INTEGER NOT NULL,
+
+    CONSTRAINT "Character_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Creature" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "imageUrl" TEXT,
+    "regionId" INTEGER,
+
+    CONSTRAINT "Creature_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Character" ADD CONSTRAINT "Character_regionId_fkey" FOREIGN KEY ("regionId") REFERENCES "Region"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Character" ADD CONSTRAINT "Character_raceId_fkey" FOREIGN KEY ("raceId") REFERENCES "Race"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Creature" ADD CONSTRAINT "Creature_regionId_fkey" FOREIGN KEY ("regionId") REFERENCES "Region"("id") ON DELETE SET NULL ON UPDATE CASCADE;
--- tes-backend/src/controllers/charactersController.ts
+++ tes-backend/src/controllers/charactersController.ts
@@ -3,6 +3,16 @@ import { FastifyRequest, FastifyReply } from "fastify";
 // Début du typage
 type GetByIdRequest = FastifyRequest<{ Params: { id: string } }>;
 
+type CreateCharacterRequest = FastifyRequest<{
+  Body: {
+    name: string;
+    description: string;
+    imageUrl?: string;
+    regionId: number;
+    raceId: number;
+  };
+}>;
+
 // Fin du typage
 
 export async function getAllCharacters(
@@ -36,5 +46,42 @@ export async function getCharacterById(
   } catch (error: any) {
     console.error("Erreur lors de la récupération du charactère :", error);
     reply.status(500).send({ error: "Erreur interne du serveur" });
+  }
+}
+
+export async function createCharacter(
+  request: CreateCharacterRequest,
+  reply: FastifyReply
+) {
+  const { name, description, imageUrl, regionId, raceId } = request.body;
+
+  if (!name || !description || !regionId || !raceId) {
+    return reply.status(400).send({
+      error:
+        "Les champs name, description, regionId et raceId sont obligatoires",
+    });
+  }
+
+  try {
+    const character = await request.server.prisma.character.create({
+      data: {
+        name,
+        description,
+        imageUrl: imageUrl || null,
+        region: {
+          connect: { id: regionId },
+        },
+        race: {
+          connect: { id: raceId },
+        },
+      },
+    });
+
+    return reply.status(201).send(character);
+  } catch (error: any) {
+    return reply.status(500).send({
+      error: "Impossible de créer le personnage.",
+      details: error.message,
+    });
   }
 }
