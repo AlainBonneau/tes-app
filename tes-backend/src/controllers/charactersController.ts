bf35e46df4049d4bab28812bbf5cff7ca3c00c14
Alain Bonneau <alaindev97@gmail.com>
2025-06-04 02:25:29 +0200

    add get character by id controller

--- tes-backend/src/controllers/charactersController.ts
+++ tes-backend/src/controllers/charactersController.ts
@@ -1,6 +1,7 @@
 import { FastifyRequest, FastifyReply } from "fastify";
 
 // Début du typage
+type GetByIdRequest = FastifyRequest<{ Params: { id: string } }>;
 
 // Fin du typage
 
@@ -13,6 +14,27 @@ export async function getAllCharacters(
     reply.send(characters);
   } catch (error: any) {
     console.error("Erreur lors de la récupération des charactères :", error);
+    reply.status(500).send({ error: "Erreur interne du serveur" });
+  }
+}
+
+export async function getCharacterById(
+  request: GetByIdRequest,
+  reply: FastifyReply
+) {
+  try {
+    const id = parseInt(request.params.id, 10);
+    const character = await request.server.prisma.character.findUnique({
+      where: { id },
+    });
+
+    if (!character) {
+      return reply.status(404).send({ error: "Charactère introuvable" });
+    }
+
+    reply.send(character);
+  } catch (error: any) {
+    console.error("Erreur lors de la récupération du charactère :", error);
     reply.status(500).send({ error: "Erreur interne du serveur" });
   }
 }
