245e1b746ecdd123a6e7298d412682789dbcce0b
Alain Bonneau <alaindev97@gmail.com>
2025-06-17 01:35:00 +0200

    custom navbar

--- tes-frontend/src/app/components/Navbar.tsx
+++ tes-frontend/src/app/components/Navbar.tsx
@@ -6,7 +6,7 @@ export default function Navbar() {
   return (
     <header>
       <nav className="bg-dark text-parchment border-y-4 border-gold px-6 py-4 shadow-lg flex justify-center items-center tracking-wider">
-        <div className="flex gap-6 text-lg font-uncial">
+        <div className="flex items-center gap-6 font-uncial uppercase">
           <Link href="/" className="hover:text-gold transition underline">
             Accueil
           </Link>
@@ -23,7 +23,7 @@ export default function Navbar() {
           <Link href="/tavern" className="hover:text-gold transition">
             Taverne
           </Link>
-          <Link href="/login" className="hover:text-gold transition">
+          <Link href="/login" className="bg-blood pt-2 pb-2 pl-4 pr-4 rounded-lg border border-gold hover:text-gold transition">
             Connexion
           </Link>
         </div>
