905f566051487c222d449b4b51af801582ade4eb
Alain Bonneau <alaindev97@gmail.com>
2025-06-18 02:38:01 +0200

    init homepage

--- tes-frontend/src/app/page.tsx
+++ tes-frontend/src/app/page.tsx
@@ -1,10 +1,27 @@
 "use client";
 import { motion } from "framer-motion";
+import backgroundImage from "../../public/assets/bg.jpg";
 
 export default function Home() {
   return (
     <main>
-      <section className="top-section h-screen w-full border"></section>
+      <section
+        className="top-section h-screen w-full bg-center bg-no-repeat bg-cover bg-black/60
+    bg-blend-overlay"
+        style={{ backgroundImage: `url(${backgroundImage.src})` }}
+      >
+        <div className="title-container font-cinzel">
+          <h1 className="font-uncial text-gold text-5xl">
+            Explorez
+            <br /> L&apos;univers de
+            <br /> Tamriel
+          </h1>
+          <p>
+            Bestiaire, Personnage, Lieux et secrets de l&apos;univers The Elder
+            Scroll
+          </p>
+        </div>
+      </section>
     </main>
   );
 }
