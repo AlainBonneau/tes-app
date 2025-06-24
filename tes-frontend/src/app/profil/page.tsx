7c986090ecb76242b2294183714deba040f5dc8f
Alain Bonneau <alaindev97@gmail.com>
2025-06-24 06:40:57 +0200

    add login page

---
+++ tes-frontend/src/app/profil/page.tsx
@@ -0,0 +1,11 @@
+export default function ProfilePage() {
+  return (
+    <div className="profile-container bg-gold min-h-screen">
+      <div className="bg-blood h-[20vh] w-full flex items-center justify-center">
+        <h1 className="text-3xl md:text-4xl font-uncial uppercase text-gold text-center">
+          Profil
+        </h1>
+      </div>
+    </div>
+  );
+}
