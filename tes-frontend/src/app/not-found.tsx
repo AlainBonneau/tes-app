7b4e71dd55663602622bdb5706fc4d42e4e88e99
Alain Bonneau <alaindev97@gmail.com>
2025-06-26 08:33:38 +0200

    costom not found page (404)

--- tes-frontend/src/app/not-found.tsx
+++ tes-frontend/src/app/not-found.tsx
@@ -4,18 +4,25 @@ import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gold text-blood">
-      <h1 className="text-5xl font-bold mb-4">404</h1>
-      <h2 className="text-2xl mb-2">Page introuvable</h2>
-      <p className="mb-8">
-        La page que vous cherchez n’existe pas ou a été déplacée.
-      </p>
-      <Link
-        href="/"
-        className="bg-blood text-gold py-2 px-6 rounded font-cinzel font-bold hover:bg-blood/80 transition"
-      >
-        Retour à l’accueil
-      </Link>
+    <div className="min-h-screen flex flex-col items-center  bg-gold text-blood">
+      <div className="bg-blood h-[20vh] w-full flex items-center justify-center mb-8">
+        <h1 className="text-3xl md:text-4xl font-cinzel font-bold uppercase text-gold text-center">
+          404
+        </h1>
+      </div>
+      <div className="page-container w-full flex flex-col items-center justify-center text-center p-8">
+        <h2 className="text-5xl font-bold mb-4">404</h2>
+        <h3 className="text-2xl mb-2">Page introuvable</h3>
+        <p className="mb-8">
+          La page que vous cherchez n’existe pas ou a été déplacée.
+        </p>
+        <Link
+          href="/"
+          className="bg-blood text-gold py-2 px-6 rounded font-cinzel font-bold hover:bg-blood/80 transition"
+        >
+          Retour à l’accueil
+        </Link>
+      </div>
     </div>
   );
 }
