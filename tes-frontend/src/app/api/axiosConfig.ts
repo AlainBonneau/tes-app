b6b842f5cebba2e3c3022ee65f0ad7b4a822e9c6
Alain Bonneau <alaindev97@gmail.com>
2025-07-24 12:26:32 +0200

    feat: add env api url

--- tes-frontend/src/app/api/axiosConfig.ts
+++ tes-frontend/src/app/api/axiosConfig.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
+import "dotenv/config";
 
 const api = axios.create({
-  baseURL: "http://localhost:3001/",
+  baseURL: process.env.API_URL || "http://localhost:3001",
   withCredentials: true,
 });
 
